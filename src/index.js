5ca9ffa8538749427decad6f3dbfee6ba7587b93
quangdat385 <95111284+quangdat385@users.noreply.github.com>
2022-07-14 22:58:24 +0700

    all

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import{BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import Expenses from './pages/expenses';
+import Invoices from './pages/invoices'
+import Invoice from './pages/invoice'
 import reportWebVitals from './reportWebVitals';
+
+//Tao Router bao quanh
+//NavLink tao active
+//outlet noi route con display
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>}>
+          <Route path="invoices" element={<Invoices/>}>
+          <Route
+            index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
+            <Route path=":invoiceId" element={<Invoice/>}></Route>
+          </Route>
+          <Route path="expenses" element={<Expenses/>}></Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+          </Route>
+        
+      </Routes>
+      
+    </Router>
+
   </React.StrictMode>
 );
 
